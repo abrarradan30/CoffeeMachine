070bf0d880569877fddf46497239e8a8fddbedad
abrarradan30 <abradan84@gmail.com>
2023-10-14 20:00:51 +0700

    export and import

--- index.js
+++ index.js
@@ -5,3 +5,17 @@ console.log("Mencampurkan air dan kopi");
 console.log("Menuangkan kopi ke dalam gelas");
 console.log("Menuangkan susu ke dalam gelas");
 console.log("Kopi Anda sudah siap!");
+
+const coffeeStock = require('./state.js');
+ 
+console.log(coffeeStock);
+
+const makeCoffee = (type, miligrams) => {
+    if (coffeeStock[type] >= miligrams) {
+      console.log("Kopi berhasil dibuat!");
+    } else {
+      console.log("Biji kopi habis!");
+    }
+  }
+   
+makeCoffee("robusta", 80);
